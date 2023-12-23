9638bdbbb74a3d48dcf7d746c1b18370f65c05bc
Sujal-Yadav <sujjalyadav@gmail.com>
2023-12-23 20:04:10 +0530

    Introduce Share Blog

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const { ObjectId } = require('mongoose').Types;
 const User = require('./models/user');
 const Blog = require('./models/blog');
 const session = require('express-session');
-// const isBlogAuthor = require('./models/author');
 const path = require('path');
 const fs = require('fs');
 const { timeStamp } = require('console');
@@ -156,40 +155,41 @@ app.get('/getblogs/:id', isAuthenticated, async (req, res) => {
         let posts = "";
         array.forEach(element => {
             posts += `
-                <article class="flex max-w-xl flex-col items-start justify-evenly items-stretch border-solid border-2 border-black rounded-md p-8">
-                    <div class="flex items-center gap-x-4 text-xs">
-                        <time datetime="2020-03-16" class="text-gray-500">${element.time}</time>
+                    <article class="flex max-w-xl flex-col items-start justify-evenly items-stretch border-solid border-2 border-black rounded-md p-8">
+                        <div class="flex items-center gap-x-4 text-xs">
+                            <time datetime="2020-03-16" class="text-gray-500">${element.time}</time>
 
-                    </div>
-                    <div class="group relative">
-                        <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                            <a href="#">
-                                <span class="absolute inset-0"></span>
-                                ${element.title}
-                            </a>
-                        </h3>
-                        <div class="mt-5 overflow-hidden line-clamp-3 text-sm leading-6 text-gray-600">
-                        <p >${element.about}</p>
                         </div>
-                    </div>
-                    <div class="relative mt-8 flex items-center gap-x-4">
-                        <img src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                            alt="" class="h-10 w-10 rounded-full bg-gray-50">
-                        <div class="text-sm leading-6">
-                            <p class="font-semibold text-gray-900">
+                        <div class="group relative">
+                            <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
                                 <a href="#">
                                     <span class="absolute inset-0"></span>
-                                    ${element.fullname}
+                                    ${element.title}
                                 </a>
-                            </p>
-                            <p class="text-gray-600">${element.profession}</p>
+                            </h3>
+                            <div class="mt-5 overflow-hidden line-clamp-3 text-sm leading-6 text-gray-600">
+                            <p >${element.about}</p>
+                            </div>
                         </div>
-                    </div>
-                    <div class="grid grid-cols-2  gap-2 mt-3  text-left text-white">
-                        <a href="/readBlog/${element._id}" class="btn col-span-1 text-white hover:bg-[#3b82f6] bg-[#1d4ed8]">Read</a>
-                        <a href="/shareBlog/${element._id}" class="btn col-span-1 text-white hover:bg-[#3b82f6] bg-[#1d4ed8]">Share</a>
-                    </div>
-                </article>`
+                        <div class="relative mt-8 flex items-center gap-x-4">
+                            <img src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                alt="" class="h-10 w-10 rounded-full bg-gray-50">
+                            <div class="text-sm leading-6">
+                                <p class="font-semibold text-gray-900">
+                                    <a href="#">
+                                        <span class="absolute inset-0"></span>
+                                        ${element.fullname}
+                                    </a>
+                                </p>
+                                <p class="text-gray-600">${element.profession}</p>
+                            </div>
+                        </div>
+                        <div class="grid grid-cols-2  gap-2 mt-3  text-left text-white">
+                            <a href="/readBlog/${element._id}" class="btn col-span-1 text-white hover:bg-[#3b82f6] bg-[#1d4ed8]">Read</a>
+                            <a href="#" class="btn col-span-1 text-white hover:bg-[#3b82f6] bg-[#1d4ed8]" onclick="copyToClipboard('${element._id}')">Share</a>
+
+                        </div>
+                    </article>`
         });
         // Replace the placeholder with the serialized data
         let modifiedhtml = htmlFile.replace('{ blogPosts }', posts);
@@ -203,24 +203,23 @@ app.get('/getblogs/:id', isAuthenticated, async (req, res) => {
     }
 })
 
-app.get('/shareBlog', async (req, res) => {
-    const blogId = req.query.id;
+app.get('/shareBlog/:id', async (req, res) => {
+    const blogId = req.params.id;
+
     try {
-        // Check if the Web Share API is supported
-        if (navigator.share) {
-          await navigator.share({
-            title: 'Your Blog Post Title',
-            text: 'Check out this interesting blog post!',
-            url: window.location.href,
-          });
-        } else {
-          // Fallback for browsers that do not support the Web Share API
-          alert('Web Share API is not supported on this browser.');
+        const blog = await Blog.findById(blogId);
+
+        if (!blog) {
+            return res.status(404).json({ error: 'Blog not found' });
         }
-      } catch (error) {
-        console.error('Error sharing:', error.message);
-      }
-})
+
+        const blogLink = `http://localhost:4000/readBlog/${blogId}`;
+        res.json({ blogLink });
+    } catch (error) {
+        console.error('Error fetching blog:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
 app.post('/profile/addblog', isAuthenticated, async (req, res) => {
     const blog = req.params.blog;
